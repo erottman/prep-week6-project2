484c16ee6ec5edc53bb1b03db3ee31e7976903a0
Erica Rottman <erica7650@gmail.com>
2016-12-27 08:22:59 -0800

    complete

--- main.js
+++ main.js
@@ -37,13 +37,13 @@ $(".alertme").click(function() {
 
 
 //3.
+/*
+$(document).ready(function() {
 
-$(document).ready(function () {
-
-  $(".addStyle").click(function () {
-    $(".addStyle").addClass("buttonStyle");
-  });
-});
+    $(".addStyle").click(function() {
+        $(".addStyle").addClass("buttonStyle");
+    });
+});  *?
 
 
 
@@ -51,22 +51,71 @@ $(document).ready(function () {
 //4.
 
 
+/*$(document).ready(function() {
+$(".addDifferentStyle").click(function() {
+$(".addDifferentStyle").addClass("addUniqueStyle");
+});
+}); */
+
+
 //5.
+
+$(document).ready(function() {
+
+    $(".addStyle").click(function() {
+        $(".addStyle").addClass("buttonStyle");
+    });
+
+
+    $(".addDifferentStyle").click(function() {
+        $(".addDifferentStyle").addClass("addUniqueStyle");
+    });
+
+    // $("button").click(function() {
+        // $("button").removeClass();
+    // })
+
+
+
+
 
 
 //6.
 
 
+
+$(".clickToggle").click(function () {
+
+  $(".clickToggle").toggleClass("addUniqueStyle");
+});
+
+
 //7.
+
+$(".squareBtn").click(function () {
+
+  $(".square").hide();
+});
+
 
 
 
 //8.
 
+$(".circle").click(function () {
+
+  $(".circle").hide();
+});
+
 
 
 //9.
 
+$(".toggleRedSquare").click(function () {
 
+  $(".redSquare").toggle(1000);
+});
 
- // Close: $(document).ready(function() {
+});
+
+// Close: $(document).ready(function() {
